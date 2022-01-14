7d751d1e07d4f3377f2d962c176a0fc27d2689d5
uJoao22 <joaopaniciobotelho@hotmail.com>
2022-01-14 12:15:52 -0300

    Mapeando novas entidades

---
+++ loja/src/main/java/br/com/alura/loja/modelo/Pedido.java
@@ -0,0 +1,56 @@
+package br.com.alura.loja.modelo;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pedidos")
+public class Pedido {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private LocalDate data = LocalDate.now();
+	private BigDecimal valorTotal;
+	
+	@ManyToOne
+	private Cliente cliente;
+	
+	public Pedido() {}
+
+	public Pedido(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public BigDecimal getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(BigDecimal valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+	
+}
