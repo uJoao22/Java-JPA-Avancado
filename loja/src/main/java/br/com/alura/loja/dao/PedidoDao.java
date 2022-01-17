11e1ed5de0e5118efa6560583550248fd0e4ca79
uJoao22 <joaopaniciobotelho@hotmail.com>
2022-01-17 10:02:01 -0300

    Consultas com select new

--- loja/src/main/java/br/com/alura/loja/dao/PedidoDao.java
+++ loja/src/main/java/br/com/alura/loja/dao/PedidoDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import br.com.alura.loja.modelo.Pedido;
+import br.com.alura.loja.vo.RelatorioDeVendasVo;
 
 public class PedidoDao {
 
@@ -23,14 +24,16 @@ public class PedidoDao {
 		return em.createQuery(jpql, BigDecimal.class).getSingleResult();
 	}
 
-	//Este método devolve uma lista com um array de objetos, pois ele irá retornar diferentes tipos de objetos
-	public List<Object[]> relatiorioDeVendas() {
-		//Selecionando o nome da entidade produto, a soma de quantidade da entidade item e a maior data da entidade pedido, e então
-		//Fazendo o relacionamento dessas entidades a partir da entidade Pedido e agrupando pelo nome e ordenando pela quantidade
-		String jpql = "SELECT produto.nome, SUM(item.quantidade), MAX(pedido.data) FROM Pedido pedido "
+	//Este método devolve uma lista da class RelatorioDeVendasVo
+	public List<RelatorioDeVendasVo> relatiorioDeVendas() {
+		//Usando uma funcionalidade da JPA, o SELECT new, para dar new em uma class e passar para ele por parametro os dados que
+		//é desejado fazer a consulta, é necessario informar todo o caminho da class e assim será selecionado o nome da entidade 
+		//produto, a soma de quantidade da entidade item e a maior data da entidade pedido, e então fazendo o relacionamento dessas 
+		//entidades a partir da entidade Pedido e agrupando pelo nome e ordenando pela quantidade
+		String jpql = "SELECT new br.com.alura.loja.vo.RelatorioDeVendasVo(produto.nome, SUM(item.quantidade), MAX(pedido.data)) FROM Pedido pedido "
 				+ " JOIN pedido.itens item JOIN item.produto produto "
 				+ " GROUP BY produto.nome ORDER BY item.quantidade DESC";
-		return em.createQuery(jpql, Object[].class).getResultList();
+		return em.createQuery(jpql, RelatorioDeVendasVo.class).getResultList();
 	}
 	
 }
--- loja/src/main/java/br/com/alura/loja/testes/CadastroDePedidos.java
+++ loja/src/main/java/br/com/alura/loja/testes/CadastroDePedidos.java
@@ -15,6 +15,7 @@ import br.com.alura.loja.modelo.ItemPedido;
 import br.com.alura.loja.modelo.Pedido;
 import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
+import br.com.alura.loja.vo.RelatorioDeVendasVo;
 
 public class CadastroDePedidos {
 	public static void main(String[] args) {
@@ -24,33 +25,41 @@ public class CadastroDePedidos {
 		ProdutoDao produtoDao = new ProdutoDao(em);
 		ClienteDao clienteDao = new ClienteDao(em);
 		Produto produto = produtoDao.buscarPorId(1l);
+		Produto produto2 = produtoDao.buscarPorId(2l);
+		Produto produto3 = produtoDao.buscarPorId(3l);
 		Cliente cliente = clienteDao.buscarPorId(1l);
 	
 		em.getTransaction().begin();
 		
 		Pedido pedido = new Pedido(cliente);
-		
 		pedido.adicionarItem(new ItemPedido(10, pedido, produto));
+		pedido.adicionarItem(new ItemPedido(40, pedido, produto2));
+
+		Pedido pedido2 = new Pedido(cliente);
+		pedido2.adicionarItem(new ItemPedido(2, pedido, produto3));		
 		
 		PedidoDao pedidoDao = new PedidoDao(em);
 		pedidoDao.cadastrar(pedido);
+		pedidoDao.cadastrar(pedido2);
 		
 		em.getTransaction().commit();
 
 		BigDecimal totalVendido = pedidoDao.valorTotalVendido();
 		System.out.println("VALOR TOTAL: "+totalVendido);
 		
-		List<Object[]> relatorio = pedidoDao.relatiorioDeVendas();
-		for (Object[] obj : relatorio) {
-			System.out.println(obj[0]);
-			System.out.println(obj[1]);
-			System.out.println(obj[2]);
-		}
+		List<RelatorioDeVendasVo> relatorio = pedidoDao.relatiorioDeVendas();
+		//Imprimindo todas as toString que existem na class, usando o forEach 
+		relatorio.forEach( System.out::println );
 	}
 	
 	private static void popularBancoDeDados() {
 		Categoria celulares = new Categoria("CELULARES");
+		Categoria videogames = new Categoria("VIDEOGAMES");
+		Categoria informatica = new Categoria("INFORMATICA");
+		
 		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares );
+		Produto videogame = new Produto("PS5", "Playstation 5", new BigDecimal("5000"), videogames);
+		Produto macbook = new Produto("Macbook", "Macbook pro", new BigDecimal("4000"), informatica);
 
 		Cliente cliente = new Cliente("João","123.456.789-10");
 		
@@ -62,7 +71,13 @@ public class CadastroDePedidos {
 		em.getTransaction().begin();
 		
 		categoriaDao.cadastrar(celulares);
+		categoriaDao.cadastrar(videogames);
+		categoriaDao.cadastrar(informatica);
+		
 		produtoDao.cadastrar(celular);
+		produtoDao.cadastrar(videogame);
+		produtoDao.cadastrar(macbook);
+		
 		clienteDao.cadastrar(cliente);
 		
 		em.getTransaction().commit();
