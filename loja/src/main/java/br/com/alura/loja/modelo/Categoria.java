6a0dd47f17e09f5903310c42bc17bd740c9f54e8
uJoao22 <joaopaniciobotelho@hotmail.com>
2022-01-18 11:54:18 -0300

    Mapeamento de chaves compostas

--- loja/src/main/java/br/com/alura/loja/modelo/Categoria.java
+++ loja/src/main/java/br/com/alura/loja/modelo/Categoria.java
@@ -1,5 +1,6 @@
 package br.com.alura.loja.modelo;
 
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,24 +10,20 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "categorias")
 public class Categoria {
+//Tratando o id dessa entidade como se fosse um id composto
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String nome;
+	//Usando essa anotação para dizer que a linha abaixo é a chave primaria da entidade
+	@EmbeddedId
+	private CategoriaId id;
 	
 	public Categoria() {
 	}
 	
 	public Categoria(String nome) {
-		this.nome = nome;
+		this.id = new CategoriaId(nome, "xpto");
 	}
-
+	
 	public String getNome() {
-		return nome;
+		return this.id.getNome();
 	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
 }
